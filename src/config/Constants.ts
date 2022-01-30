f6a87afda03d8912fe9ff6355cafb989827b82ac
Chrystiomar <chrystiomar@hotmail.com>
2022-01-30 08:48:01 -0300

    finalizando projeto

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+	'mongodb+srv://dio:<dio>@encurta-url.0mjms.mongodb.net/url-shortner?retryWrites=true&w=majority',
+}
